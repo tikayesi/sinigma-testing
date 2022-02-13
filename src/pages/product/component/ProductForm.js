f6269d4824128ff7364eff47c9ded2cad9125e48
tika.kristiani <tika.kristiani@gmail.com>
2022-02-13 22:42:11 +0700

    able using formik yup

--- src/pages/product/component/ProductForm.js
+++ src/pages/product/component/ProductForm.js
@@ -1,7 +1,8 @@
-import axios from "axios";
-import React, {useEffect, useRef, useState} from "react";
+import { useFormik } from "formik";
+import React, {useEffect, useState} from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { createProduct, getProduct, updateproduct } from "../services/ProductService";
+import * as Yup from "yup";
 
 export const ProductForm = () =>{
     let params = useParams();
@@ -9,73 +10,89 @@ export const ProductForm = () =>{
     const [newName, setNewName] = useState('');
     const readable = params.id ? true : false;
 
+    const formik = useFormik({
+      initialValues:{
+        id: newId,
+        name: newName
+      },
+      validationSchema : Yup.object({
+        name: Yup.string().required("Required!").min(5, "minimum 5 character"),
+    }),
+      onSubmit: () => {
+        if(params.id){
+          handleUpdate()
+        }else{
+         handleSubmit()
+        }
+      }
+    })
+
     useEffect(() => {
       if(params.id){
-        getProduct(params.id)
-        .then(res => {
-          setNewId(res.data.id);
-          setNewName(res.data.name)
-        })
+        getProductById()
     }
     }, []);
 
+    const getProductById = async() =>{
+      const res = await getProduct(params.id)
+      formik.values.id = res.data.id;
+          formik.values.name = res.data.name;
+          setNewId(formik.values.id);
+          setNewName(formik.values.name)
+    }
+
     const navigate = useNavigate();
 
-    const handleChangeId = (event) => {
-        setNewId(event.target.value)
-    }
-
-    const handleChangeName = (event) => {
-        setNewName(event.target.value)
-    }
-
-    const handleSubmit = async (event) => {
+    const handleSubmit = async () => {
         try{
-           let res = await createProduct({ id : newId, name : newName });
+           let res = await createProduct(formik.values);
             console.log(res);
             console.log(res.data);
           navigate("/products");
         } catch (error) {
           console.error(error);
         }
-        event.preventDefault()
     }
 
-    const handleUpdate = async (event) => {
+    const handleUpdate = async () => {
         try{
-            let res = await updateproduct( { id : newId, name : newName })
+            let res = await updateproduct( formik.values)
              console.log(res);
              console.log(res.data);
            navigate("/products");
          } catch (error) {
            console.error(error);
          }
-        event.preventDefault()
     }
 
         return(
             <div>
             <h2>Product Form</h2>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="form-group row">
-                <label htmlFor="inputId" className="col-sm-2 col-form-label">Id</label>
+                <label htmlFor="id" className="col-sm-2 col-form-label">Id</label>
                 <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputId" placeholder="Id"
-                value={newId}
-                onChange={e => handleChangeId(e)} readOnly={readable}/>
+                <input type="text" name="id" className="form-control" id="id" placeholder="Id"
+                value={formik.values.id}
+                onChange={formik.handleChange} readOnly={readable}/>
                 </div>
             </div>
             <br></br>
             <div className="form-group row">
-                <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+                <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
                 <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputName" placeholder="Name"
-                value={newName}
-                onChange={e => handleChangeName(e)}/>
+                <input type="text" name="name" className="form-control" id="name" placeholder="Name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}/>
+                 {formik.errors.name && formik.touched.name && (
+            <small className="text-danger">{formik.errors.name}</small>)}
                 </div>
             </div>
             <br></br>
             <Link to={"/products"}><button className="btn btn-warning">Cancel</button></Link> {' '}
-            <input className="btn btn-primary" type="submit" value="Submit" onClick={(e) => params.id ? handleUpdate(e) : handleSubmit(e)}/> 
+            <input className="btn btn-primary" type="submit" value="Submit"/> 
+            </form>
             </div>
         )
     }
